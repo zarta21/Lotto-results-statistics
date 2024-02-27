a7905f019b51d080ed08f2f6430ca883c9bbc175
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-02-27 10:54:08 +0100

    Update eurojackpotDB.js

--- Backend/eurojackpotDB.js
+++ Backend/eurojackpotDB.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const eurojackpotSchema = require("./models/eurojackpotModel")
 
-const connection4 = mongoose.createConnection(process.env.E_Lotto_DB_URL).on("error", (err) => {
+const connection2 = mongoose.createConnection(process.env.E_Lotto_DB_URL).on("error", (err) => {
     console.error(err.message);
 });
 
-// connection1.model("Product", ProductSchema)
-
-module.exports = connection4.model("Eurojackpot", eurojackpotSchema)
+module.exports = connection2.model("Eurojackpot", eurojackpotSchema)
