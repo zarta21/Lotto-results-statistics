5e8ef9f454e2d7b1a8c98db66c5b4f275f1e407e
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-02-27 09:58:36 +0100

    Create Loader.jsx

---
+++ Frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './loader.scss'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <h1>Loading...</h1>
+        <div className="preloader">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
