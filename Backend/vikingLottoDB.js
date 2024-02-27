f536b70abc136a2ba2c8a8ffd08d7066292d728a
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-02-27 10:53:47 +0100

    Update vikingLottoDB.js

--- Backend/vikingLottoDB.js
+++ Backend/vikingLottoDB.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const vikingSchema = require("./models/vikingModel")
 
-const connection3 = mongoose.createConnection(process.env.V_Lotto_DB_URL).on("error", (err) => {
+const connection1 = mongoose.createConnection(process.env.V_Lotto_DB_URL).on("error", (err) => {
     console.error(err.message);
 });
 
-// connection1.model("Product", ProductSchema)
-
-module.exports = connection3.model("Viking", vikingSchema)
+module.exports = connection1.model("Viking", vikingSchema)
